92c4b25317e96628dddd711ed08593af7664b737
Andrei Olar <andrei.olar@gmail.com>
2024-12-29 13:46:50 +0200

    extend the functionality of the Block class

--- src/matchescu/matching/blocking/_block.py
+++ src/matchescu/matching/blocking/_block.py
@@ -1,16 +1,43 @@
+import itertools
 from dataclasses import dataclass, field
-from typing import Any
-
-from matchescu.typing import EntityReference
+from typing import Any, Iterator, Callable, Iterable
 
 
 @dataclass
 class Block:
-    key: Any = field(init=True, repr=True, hash=True, compare=True)
-    references: dict[str, list[EntityReference]] = field(default_factory=dict, init=False, repr=False, hash=False, compare=False)
+    __DEFAULT_SOURCE = "DEFAULT"
 
-    def add_reference(self, source_name: str, reference: EntityReference) -> "Block":
-        refs = self.references.get(source_name) or []
-        refs.append(reference)
-        self.references[source_name] = refs
+    key: Any = field(init=True, repr=True, hash=True, compare=True)
+    references: dict[str, list[int]] = field(default_factory=dict, init=False, repr=False, hash=False, compare=False)
+
+    def __post_init__(self):
+        if self.key is None or not self.key.strip():
+            raise ValueError("invalid blocking key")
+
+    def __update_references(self, param: Any, source_name: str | None, op: Callable[[list], Callable[[Any], None]]) -> None:
+        src = source_name if source_name is not None and source_name.strip() else self.__DEFAULT_SOURCE
+        refs = self.references.get(src) or []
+        op(refs)(param)
+        self.references[src] = refs
+
+    def append(self, ref_id: int, source_name: str | None = None) -> "Block":
+        self.__update_references(ref_id, source_name, lambda x: x.append)
         return self
+
+    def extend(self, ref_ids: Iterable[int], source_name: str | None = None) -> "Block":
+        self.__update_references(ref_ids, source_name, lambda x: x.extend)
+        return self
+
+    def candidate_pairs(self) -> Iterator[tuple[int, int]]:
+        n_sources = len(self.references)
+        if n_sources < 1:
+            yield from ()
+        elif n_sources < 2:
+            for ref_ids in self.references.values():
+                for pair in itertools.combinations(ref_ids, 2):
+                    yield pair
+        else:
+            sources = list(self.references.keys())
+            for a, b in itertools.combinations(sources, 2):
+                for prod in itertools.product(self.references[a], self.references[b]):
+                    yield prod
