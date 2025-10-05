d6fae322addf3820b721716b156cf7f650892876
Andrei Olar <andrei.olar@gmail.com>
2025-10-05 14:28:52 +0300

    Move machine learning based matcher implementations under matchescu.matching.matchers (#19)
    * Move ditto to matchescu.matching.matchers.ml namespace
    
    - bump version to 0.13.0
    - notebooks that use ditto should change the imported namespace:
     matchescu.matching.ml -> matchescu.matching.matchers.ml
    
    * expose bucketed similarities through matchescu.matching.similarity package

--- src/matchescu/matching/similarity/__init__.py
+++ src/matchescu/matching/similarity/__init__.py
@@ -3,10 +3,17 @@ from matchescu.matching.similarity._exact_match import ExactMatch
 from matchescu.matching.similarity._learned_levenshtein import LevenshteinLearner
 from matchescu.matching.similarity._numeric import (
     BoundedNumericDifferenceSimilarity,
+    BucketedNorm,
     Norm,
 )
 from matchescu.matching.similarity._string import (
     StringSimilarity,
+    BucketedStringSimilarity,
+    BucketedJaccard,
+    BucketedJaro,
+    BucketedJaroWinkler,
+    BucketedLevenshteinDistance,
+    BucketedLevenshteinSimilarity,
     Jaccard,
     Jaro,
     JaroWinkler,
@@ -18,8 +25,15 @@ from matchescu.matching.similarity._string import (
 __all__ = [
     "T",
     "BoundedNumericDifferenceSimilarity",
+    "BucketedNorm",
     "Similarity",
     "ExactMatch",
+    "BucketedStringSimilarity",
+    "BucketedJaccard",
+    "BucketedJaro",
+    "BucketedJaroWinkler",
+    "BucketedLevenshteinDistance",
+    "BucketedLevenshteinSimilarity",
     "Jaccard",
     "Jaro",
     "JaroWinkler",
