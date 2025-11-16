9b6ea6dcc04730f2a871b61c3a49e52574cb25d9
Andrei Olar <andrei.olar@gmail.com>
2025-11-16 15:00:41 +0200

    implement simple diff between model config and ds config
    the settings loading logic must implement a slightly more convoluted
    attribute level diff for returning the correct setting when both
    top-level dataset and model configs are present.

--- src/matchescu/matching/matchers/ml/ditto/training/_config.py
+++ src/matchescu/matching/matchers/ml/ditto/training/_config.py
@@ -1,9 +1,8 @@
 import json
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from os import PathLike
 from pathlib import Path
-from typing import Type, Optional
-
+from typing import Type, Optional, cast
 
 DEFAULT_MODEL_DIR = Path.cwd() / "models"
 DEFAULT_DATA_DIR = Path.cwd() / "data"
@@ -30,23 +29,29 @@ class TrainingConfig(DatasetTrainingParams):
 
     @classmethod
     def __read_model_training_params[T: ModelTrainingParams](
-        cls, config: dict, clazz: Type[T]
+        cls, config: dict, parent: T | None, clazz: Type[T]
     ) -> T:
+        lr = 3e-5 if parent is None else parent.learning_rate
+        fl = 0 if parent is None else parent.frozen_layer_count
+        ep = 10 if parent is None else parent.epochs
+        bs = 32 if parent is None else parent.batch_size
         return clazz(
-            learning_rate=(float(config.get("learningRate", 3e-5))),
-            frozen_layer_count=(int(config.get("frozenLayers", 0))),
-            epochs=(int(config.get("epochs", 10))),
-            batch_size=(int(config.get("batchSize", 32))),
+            learning_rate=(float(config.get("learningRate", lr))),
+            frozen_layer_count=(int(config.get("frozenLayers", fl))),
+            epochs=(int(config.get("epochs", ep))),
+            batch_size=(int(config.get("batchSize", bs))),
         )
 
     @classmethod
     def __read_dataset_training_params[T: DatasetTrainingParams](
-        cls, config: dict, clazz: Type[T]
+        cls, config: dict, parent: T | None, clazz: Type[T]
     ) -> T:
-        result = cls.__read_model_training_params(config, clazz)
+        result = cls.__read_model_training_params(config, parent, clazz)
         if "modelConfig" in config:
             result.model_configs = {
-                key: cls.__read_model_training_params(config_node, ModelTrainingParams)
+                key: cls.__read_model_training_params(
+                    config_node, result, ModelTrainingParams
+                )
                 for key, config_node in config["modelConfig"].items()
             }
         return result
@@ -56,17 +61,28 @@ class TrainingConfig(DatasetTrainingParams):
         with open(f, "r") as fp:
             config = json.load(fp)
 
-        result = cls.__read_dataset_training_params(config, TrainingConfig)
+        result = cls.__read_dataset_training_params(config, None, TrainingConfig)
         result.dataset_names = list(map(str, config.get("datasets", [])))
         result.model_names = list(map(str, config.get("models", [])))
         if "datasetConfig" in config:
             result.dataset_configs = {
                 key: cls.__read_dataset_training_params(
-                    config_node, DatasetTrainingParams
+                    config_node, result, DatasetTrainingParams
                 )
                 for key, config_node in config["datasetConfig"].items()
             }
         return result
+
+    @staticmethod
+    def __attr_val[T](params: ModelTrainingParams, attr_name: str) -> T:
+        return cast(T, params.__getattribute__(attr_name))
+
+    def _get_attr[T](
+        self, attr_name: str, model: str, dataset: str, default_val: T
+    ) -> T:
+        cfg = self.dataset_configs.get(dataset, self)
+        val = self.__attr_val(cfg, attr_name)
+        return val if val is not None and val != default_val else default_val
 
     def get(
         self,
@@ -74,6 +90,25 @@ class TrainingConfig(DatasetTrainingParams):
         dataset: Optional[str] = None,
     ) -> ModelTrainingParams:
         cfg = self.dataset_configs.get(dataset, self)
-        if model is not None:
-            cfg = cfg.model_configs.get(model, self.model_configs.get(model, self))
+        if model is None:
+            return cfg
+
+        if model in cfg.model_configs:
+            return cfg.model_configs[model]
+
+        if model in self.model_configs:
+            base_cfg = asdict(self)
+            local_cfg = asdict(cfg)
+            remote_cfg = asdict(self.model_configs[model])
+            out = {}
+            for k, v in local_cfg.items():
+                if k == "model_configs":
+                    continue
+                base_v = base_cfg.get(k, v)
+                remote_v = remote_cfg.get(k, v)
+                if remote_v != v and remote_v != base_v:
+                    out[k] = remote_v
+                else:
+                    out[k] = v
+            cfg = ModelTrainingParams(**out)
         return cfg
--- tests/matching/matchers/ml/ditto/training/test_training_config.py
+++ tests/matching/matchers/ml/ditto/training/test_training_config.py
@@ -18,16 +18,22 @@ def test_load_from_json(config):
     assert config.dataset_names == ["abt-buy"]
     assert config.learning_rate == 0.001
     assert config.batch_size == 31
-    assert len(config.dataset_configs) == 1
-    assert config.dataset_configs["abt-buy"].batch_size == 67
-    assert len(config.dataset_configs["abt-buy"].model_configs) == 1
-    assert config.dataset_configs["abt-buy"].model_configs["roberta-base"].epochs == 1
+    assert len(config.dataset_configs) == 2
+    abt_buy_cfg = config.dataset_configs["abt-buy"]
+    assert abt_buy_cfg.batch_size == 67
+    assert len(abt_buy_cfg.model_configs) == 1
+    assert abt_buy_cfg.model_configs["roberta-base"].epochs == 1
+    fz_config = config.dataset_configs["fodors-zagat"]
+    assert fz_config.frozen_layer_count == 0
+    assert fz_config.learning_rate == 0.001
+    assert fz_config.batch_size == 32
+    assert fz_config.epochs == 3
     assert len(config.model_configs) == 1
     assert config.model_configs["distilbert-base-uncased"].epochs == 2
 
 
 @pytest.mark.parametrize(
-    "model,expected", [("distilbert-base-uncased", 2), ("roberta-base", 10)]
+    "model,expected", [("distilbert-base-uncased", 2), ("roberta-base", 15)]
 )
 def test_load_model_setting_cascade(config, model, expected):
     assert config.get(model=model).epochs == expected
@@ -43,8 +49,19 @@ def test_load_dataset_setting_cascade(config, dataset, expected):
     [
         ("abt-buy", "roberta-base", 1),
         ("abt-buy", "distilbert-base-uncased", 2),
-        ("anything", "roberta-base", 10),
+        ("anything", "roberta-base", 15),
     ],
 )
 def test_load_model_per_dataset_setting_cascade(config, model, dataset, expected):
     assert config.get(model, dataset).epochs == expected
+
+
+def test_fodors_zagat(config):
+    train_params = config.get(
+        dataset="fodors-zagat", model="huawei-noah/TinyBERT_General_6L_768D"
+    )
+
+    assert train_params.batch_size == 32
+    assert train_params.epochs == 3
+    assert train_params.learning_rate == 0.001
+    assert train_params.frozen_layer_count == 0
